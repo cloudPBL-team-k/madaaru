9ec9a983d049553fec03d57365e1850f0e77e432
nakakkkkkkknn <nakashima.kenji124@mail.kyutech.jp>
2017-10-16 21:22:19 +0900

    first commit(Template)

---
+++ Code_Scan_Test_by_ZXing/Code_Scan_Test_by_ZXingPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Code_Scan_Test_by_ZXing
+{
+    public partial class Code_Scan_Test_by_ZXingPage : ContentPage
+    {
+        public Code_Scan_Test_by_ZXingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
